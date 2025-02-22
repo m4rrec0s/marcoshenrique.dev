882d4277d94c4351119211c957db04632bb88889
Marcos Henrique Araújo <mine.rique.games@gmail.com>
2025-02-22 12:21:54 -0300

    refactor: update remotePatterns in next.config.mjs to include new hostname

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'utfs.io',
-            port: '',
-            pathname: '/**',
-          },
-          {
-            protocol: 'https',
-            hostname: 'another-example.com',
-            port: '',
-            pathname: '/**',
-          },
-        ],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'utfs.io',
+        port: '',
+        pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: '6c8fb3gvzm.ufs.sh',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'another-example.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
